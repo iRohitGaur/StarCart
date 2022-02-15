070b84967fffd2b3cf37324d80a983ca5957021e
Rohit Gaur <knotjinx@gmail.com>
2022-02-15 14:39:20 +0530

    add logout

--- header.js
+++ header.js
@@ -2,6 +2,11 @@ const headerLogoBtn = document.querySelector("#stc-logo");
 const headerWishlistBtn = document.querySelector("#stc-wishlist");
 const headerCartBtn = document.querySelector("#stc-cart");
 const headerLoginBtn = document.querySelector("#stc-login");
+const headerLogoutBtn = document.querySelector("#stc-logout");
+const userProfileBtn = document.querySelector("#stc-profile");
+const userProfileDropdown = document.querySelector(
+  ".user_profile_dropdown_menu"
+);
 
 headerLogoBtn.addEventListener("click", () => {
   window.location = "/index.html";
@@ -12,6 +17,14 @@ headerWishlistBtn.addEventListener("click", () => {
 headerCartBtn.addEventListener("click", () => {
   window.location = "/cart/cart.html";
 });
-headerLoginBtn.addEventListener("click", () => {
+headerLoginBtn?.addEventListener("click", () => {
+  window.location = "/auth/auth.html";
+});
+userProfileBtn?.addEventListener("click", () => {
+  userProfileDropdown.classList.contains("show_dropdown")
+    ? userProfileDropdown.classList.remove("show_dropdown")
+    : userProfileDropdown.classList.add("show_dropdown");
+});
+headerLogoutBtn?.addEventListener("click", () => {
   window.location = "/auth/auth.html";
 });
